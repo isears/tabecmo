c9710cd1e8f24aa1d2d88e9bb8ffdc5f4a8f6bae
isears <isaac.j.sears@gmail.com>
2023-11-11 05:18:32 -0500

    Add argument to specify size of fine-tuning dataset

--- tabecmo/modeling/cvParallel.py
+++ tabecmo/modeling/cvParallel.py
@@ -8,11 +8,17 @@ import torch
 from tabecmo.modeling.cvUtil import do_cv, do_loo_cv
 from tabecmo.modeling.emrAutoencoder import EmrAutoencoder, EncoderClassifier
 import argparse
+import numpy as np
 
-
-def cv_one_model(model_name):
+def cv_one_model(args):
+    model_name, fine_tuning_size = args
     X_ecmo = torch.load("cache/ihmtensors/X_ecmo.pt").float()
     y_ecmo = torch.load("cache/ihmtensors/y_ecmo.pt").float()
+
+    if fine_tuning_size > 0:
+        indices = np.random.choice(range(0, X_ecmo.shape[0]), size=fine_tuning_size, replace=False)
+        X_ecmo = X_ecmo[indices]
+        y_ecmo = y_ecmo[indices]
 
     if model_name is not None:
         autoencoder = EmrAutoencoder.load_from_checkpoint(
@@ -33,14 +39,24 @@ def argparse_setup():
         '-n',
         type=int,
         default=0,
-        dest='n'
+        dest='n',
+        help='pretraining size (requires existance of pretrained model @ n)'
     )
 
     parser.add_argument(
         '-s',
         type=int,
         default=42,
-        dest='s'
+        dest='s',
+        help='random seed'
+    )
+
+    parser.add_argument(
+        '-m',
+        type=int,
+        default=0,
+        dest='m',
+        help='fine tuning size'
     )
 
     return parser.parse_args()
@@ -73,9 +89,9 @@ if __name__ == "__main__":
     futures = list()
 
     with ProcessPoolExecutor(max_workers=5) as executor:
-        args = [(v) for _, v in available_autoencoders.items()]
+        args = [(v, cline_args.m) for _, v in available_autoencoders.items()]
         args += [
-            None
+            (None, cline_args.m)
         ]  # last model (ECMO w/out pretraining) will not load a saved autoencoder
         result = executor.map(cv_one_model, args)
 
